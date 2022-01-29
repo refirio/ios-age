3787296af8b825a4f707c0f293e29a833b7eb0cc
refirio <refirio@gmail.com>
2022-01-29 15:16:55 +0900

    Initial Commit

---
+++ age/ageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ageApp.swift
+//  age
+//
+//  Created by refirio.
+//
+
+import SwiftUI
+
+@main
+struct ageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
